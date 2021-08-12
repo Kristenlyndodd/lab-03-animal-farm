a878db1dfd1d596bb0784b97808fead395f372b1
kristenlynodd <kristenlyndodd@gmail.com>
2021-08-12 09:43:03 -0700

    add header function

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // import functions and grab DOM elements
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
+const horseSound = document.getElementById('horse-sound');
 
-// initialize state
+const dogImage = document.getElementById('dog-pic');
+const catImage = document.getElementById('cat-pic');
+const horseImage = document.getElementById('horse-pic');
+
+const animalHeard = document.getElementById('animal-heard');
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+dogImage.addEventListener('click', () => {
+    dogSound.play();
+    animalHeard.textContent = 'Dog';
+});
+
+catImage.addEventListener('click', () => {
+    catSound.play();
+    animalHeard.textContent = 'Cat';
+
+});
+
+horseImage.addEventListener('click', () => {
+    horseSound.play();
+    animalHeard.textContent = 'Horse';
+
+});
